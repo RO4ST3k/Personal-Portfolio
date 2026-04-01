6ecd56b83b7afc0ac8b7b7b96bf503a5539c8494
RA Raganit <165394249+RO4ST3k@users.noreply.github.com>
2026-04-01 22:00:12 +0800

    Add files via upload

--- src/components/navigation_bar.tsx
+++ src/components/navigation_bar.tsx
@@ -1,17 +1,15 @@
 import { NavItems } from "./nav_items"
+import './navigation_bar.css'
 
 export function NavigationBar() {
   return (
-    <div>
-      <div>
-        <h1>Navigation Bar</h1>
-
-        <div>
+    <nav className="navbar">
+      <span className="navbar__logo">RA<span className="navbar__logo-dot">.</span></span>
+      <div className="navbar__links">
         <NavItems title="Home" path="/" />
         <NavItems title="Projects" path="/projects" />
         <NavItems title="About Me" path="/about-me" />
-        </div>
       </div>
-    </div>
+    </nav>
   );
 }
